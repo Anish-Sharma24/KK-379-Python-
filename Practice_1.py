0abee81d45e31a8527d3846373a6b89221fd08e7
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-02-16 23:21:13 +0530

    ads

--- Practice_1.py
+++ Practice_1.py
@@ -1,14 +1,14 @@
 # take input of two numbers and print all the numbers between them.
-number_one = (input ("Enter First Number = "))
-number_two = (input("Enter Second Number = "))
-if number_one != number_two:
-    if number_one < number_two:
-        while number_one <= number_two:
-            print(number_one)
-            number_one = number_one + 1
+a = (input ("Enter First Number = "))
+b = (input("Enter Second Number = "))
+if a != b:
+    if a < b:
+        while a <= b:
+            print(a)
+            a = a + 1
     else:
-        while number_two <= number_one:
-            print(number_two)
-            number_two = number_two + 1
+        while b <= a:
+            print(b)
+            b = b + 1
 else:
     print("Please give range")
