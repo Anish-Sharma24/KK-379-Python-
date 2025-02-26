725534c5b5febecf1f816736bf70aff5aa94985f
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-02-27 00:47:22 +0530

    For loops

--- PQ6.py
+++ PQ6.py
@@ -1,1 +1,5 @@
 # Learning For Loop in Python
+# Print every items of the list using from loop
+fruits = ["Apple","Mango","Cherry","Bananas","Orange","Chalta","Pineapple"]
+for x in fruits:
+    print (x)
