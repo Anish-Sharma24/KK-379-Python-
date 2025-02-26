82eaa8750b67c64f4656e078c96d9e7969f545a4
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-02-27 00:59:52 +0530

    For loop

--- PQ6.py
+++ PQ6.py
@@ -3,3 +3,13 @@
 fruits = ["Apple","Mango","Cherry","Bananas","Orange","Chalta","Pineapple"]
 for x in fruits:
     print (x)
+print ("=================================================================================")
+for e in "Mango Cherry":
+    print (e)
+
+print ("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+players = ["Virat Kohli","Rohit Sharma", "Joe Root", "Mustafizur Rahman", "Rashid Khan"]
+country = ["India", "Australia", "England", "Bangladesh", "Afghanistan"]
+for pc in players:
+    for cp in country:
+        print (pc,cp)
