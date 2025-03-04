39df7bb1d43ad035988160afff75443fecb7be7d
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-03-04 20:45:43 +0530

    done

---
+++ mod2q5.py
@@ -0,0 +1,7 @@
+# Find out the sum of numbers from 1 to 50.
+a = 0
+sum = 0
+while a <= 50:
+    sum = sum + a
+    a = a+ 1
+print (sum)
---
+++ mod2q6.py
@@ -0,0 +1,5 @@
+a = int (input ("Enter any number for multiplication table "))
+no = 1
+while no <= 10:
+    print (a * no)
+    no = no + 1
