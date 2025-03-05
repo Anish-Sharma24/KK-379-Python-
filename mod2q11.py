46f8555deb7f4f7964efd93df92df78a6051b79e
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-03-05 21:07:10 +0530

    correctly done

--- mod2q11.py
+++ mod2q11.py
@@ -3,7 +3,6 @@ number = int (input ("Enter a number which you want to make REVERSE NUMBER "))
 reversenumber = 0
 while number != 0:
     b = number % 10
-    reversenumber = (b + reversenumber) * 10
-#    print (reversenumber)
+    reversenumber = (reversenumber * 10) + b
     number = number // 10
 print (reversenumber) # issue is how to remove zero?
