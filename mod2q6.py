b2ddebe234038bb7257b59d61ef40cc4a78f777b
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-03-06 01:25:53 +0530

    prime

--- mod2q6.py
+++ mod2q6.py
@@ -1,3 +1,4 @@
+# Write a Python programme that print multiplication table of a given number. Take user input.
 a = int (input ("Enter any number for multiplication table "))
 no = 1
 while no <= 10:
