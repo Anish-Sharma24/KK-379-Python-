bc10b392efaa511a369238d05877a9b4ec4613b7
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-03-06 11:35:07 +0530

    lower case into upper case...

---
+++ mod2q4.py
@@ -0,0 +1,4 @@
+# Assign a complete sentence into a variable. Convert each small letter into a capital letter and print the output.
+sentence = input ("Write a sentence : ")
+if sentence.isupper() == False:
+    print ("Current sentence : ",sentence.upper())
