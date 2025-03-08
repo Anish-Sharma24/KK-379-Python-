02f72d41366ff953a54ec578a5b06a605ed7dd37
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-03-08 15:04:42 +0530

    dictionary

--- mod2q2.py
+++ mod2q2.py
@@ -1,1 +1,8 @@
 # Create a dictionary using the following elements 1:"Apu", 2:"Lili", 3:"Sumit", 4:"Dinesh". Change the value of 4 with your own name.
+mydictionary = {1: "Apu", 2: "Lili", 3: "Sumit", 4: "Dinesh"}
+print (mydictionary)
+print (mydictionary [1], mydictionary [4])
+print (mydictionary [2])
+print (mydictionary [3])
+mydictionary [4] = input ("Enter your name : ")
+print (mydictionary)
