9e181abaa9ad91541792085116230ac9c83e75b4
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-03-12 13:10:10 +0530

    try

--- Project Euler's Problems/ID3.py
+++ Project Euler's Problems/ID3.py
@@ -1,3 +1,13 @@
 # Largest Prime Factor
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143?
+mainloop = 2
+subloop = 2
+largest = 0
+while mainloop <= 600851475143:
+    if 600851475143 % mainloop == 0:
+        while subloop <= mainloop - 1:
+            if mainloop % subloop == 0:
+                break
+            subloop = subloop + 1
+    mainloop = mainloop + 1
