f5fe9688118f1c51aec7deebc08193bd59258293
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-04-12 18:54:12 +0530

    asdasdasd

--- Project Euler's Problems/ID3.py
+++ Project Euler's Problems/ID3.py
@@ -4,13 +4,16 @@
 mainloop = 2
 basicstore = 0
 largerstore = 0
-while mainloop <= 600851475143:
-    if 600851475143 % mainloop == 0:
+number = int (input ("Enter the number = "))
+while mainloop < number:
+    if number % mainloop == 0:
         subloop = 2
         while subloop <= mainloop - 1:
-            if mainloop % subloop != 0:
+            if mainloop % subloop == 0:
+                break
+            if mainloop > basicstore:
                 basicstore = mainloop
             subloop = subloop + 1
+            print (mainloop)
     mainloop = mainloop + 1
-if mainloop > basicstore:
-    largerstore = mainloop
+print (basicstore)
