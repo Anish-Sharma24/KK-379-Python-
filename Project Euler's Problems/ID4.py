21e288ae475b98a04312f04588c80c632431ac78
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-04-27 23:37:58 +0530

    understood

--- Project Euler's Problems/ID4.py
+++ Project Euler's Problems/ID4.py
@@ -5,15 +5,15 @@ loop1 = 100
 loop2 = 100
 number = 0
 q = 0
-reversenumber = 0
 while loop1 < 1000:
     while loop2 < 1000:
         number = loop1 * loop2
+        numbercopy = number
+        reversenumber = 0
         while number != 0:
             q = number % 10
             reversenumber = (reversenumber * 10) + q
             number = number // 10
+        print ("Original No. ", numbercopy, "Reverse No. ", reversenumber)
         loop2 = loop2 + 1
-        if reversenumber == number:
-            print (reversenumber)
     loop1 = loop1 + 1
